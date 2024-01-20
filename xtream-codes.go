9fb466caffa4f9b82e041b71ca3e4d7e5e19c8f0
philipp-ingelbach <154241722+philipp-ingelbach@users.noreply.github.com>
2024-01-20 14:37:10 +0100

    Update xtream-codes.go

--- xtream-codes.go
+++ xtream-codes.go
@@ -237,7 +237,7 @@ func (c *XtreamClient) GetSeriesInfo(seriesID string) (*Series, error) {
 
 	seriesInfo := &Series{}
 
-	jsonErr := json.Unmarshal(seriesData, &seriesInfo)
+	_ := json.Unmarshal(seriesData, &seriesInfo)
 
 	return seriesInfo, nil
 }
